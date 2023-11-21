dd77e25d6a7bc716fd48ff87b0def0320ca26791
abbyjo <abbyjo225@gmail.com>
2023-11-21 00:12:21 -0500

    Added contact form validation

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,6 +4,7 @@ export default function Contact(props) {
   let [nameInput, setName] = useState('');
   let [emailInput, setEmail] = useState('');
   let [messageInput, setMessage] = useState('');
+  let [errorMessage, setError] = useState('');
 
   const formHandler = (event) => {
     event.preventDefault();
@@ -24,6 +25,31 @@ export default function Contact(props) {
     setMessage(event.target.value)
   };
 
+  const nameBlur = (event) => {
+    if (!event.target.value){
+      setError('Name is required to leave a message!')
+    } else{
+      setError('')
+    }
+  };
+
+  const emailBlur = (event) => {
+    const regex= /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    const email= event.target.value;
+    if (!regex.test(email)){
+      setError('Please enter a valid email address!')
+    } else{
+      setError('')
+    }
+  };
+
+  const messageBlur = (event) => {
+    if (!event.target.value){
+      setError('Please refrain from sending empty messages...')
+    } else{
+      setError('')
+    }
+  };
 
   return (
     <>
@@ -31,17 +57,21 @@ export default function Contact(props) {
       <form onSubmit={formHandler} className='container' >
         <div className='m-3 row'>
           <label className='p-2'>Name</label>
-          <input className='p-2 custom-input' type='text' name='name' value={nameInput} onChange={nameChange} />
+          <input className='p-2 custom-input' type='text' name='name' value={nameInput} onChange={nameChange} onBlur={nameBlur} />
         </div>
         <div className='m-3 row'>
           <label className='p-2'>Email</label>
-          <input className='p-2 custom-input' type='text' name='email' value={emailInput} onChange={emailChange} />
+          <input className='p-2 custom-input' type='text' name='email' value={emailInput} onChange={emailChange} onBlur={emailBlur}/>
         </div>
         <div className='m-3 row'>
           <label className='p-2'>Message</label>
-          <textarea className='p-2 custom-input' placeholder="Please leave your message here!" name='message' value={messageInput} onChange={messageChange}>
+          <textarea className='p-2 custom-input' placeholder="Please leave your message here!" name='message' value={messageInput} onChange={messageChange} onBlur={messageBlur}>
           </textarea>
         </div>
+        { errorMessage ? (
+          <p className='m-3 mt-5 alert alert-danger myAlert'> {errorMessage}</p>
+        ) : ( <p></p>)
+        }
         <div className='m-5'>
           <button className='p-2 custom-btn'>Submit</button>
         </div>
