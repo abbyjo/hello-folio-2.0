0edc42541a9df330ac94595d7114ec65077c5336
abbyjo <abbyjo225@gmail.com>
2023-11-20 22:43:55 -0500

    Added links to folio page

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,8 +1,13 @@
+import '../styles/Folio.css'
+
 export default function Project(props) {
     return (
-      <div className="col p-2 mt-3">
-        <p>{props.title}</p>
-        <img src={props.src} className="rounded" width='100%'></img>
+      <div className="col p-2 mt-3 folio-div">
+        <img src={props.src} className="folio-img" width='100%'></img>
+        <div className="middle">
+          <a href={props.link} target="_blank" rel="noopener noreferrer"><h4 className="custom-label" >{props.title}</h4></a>
+          <a href={props.github} target="_blank" rel="noopener noreferrer"><img src="../src/assets/github-mark.png" className="git-icon"></img></a>
+        </div>
       </div>
     );
   }
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -5,15 +5,39 @@ export default function Portfolio() {
     <div className="container text-center">
       <h2>Portfolio</h2>
       <div className="row">
-        <Project title="Book Nite" src="https://github.com/abbyjo/book-nite/raw/main/assets/images/screenshot.png" />
-        <Project title="Leap To Success" src="https://i.gyazo.com/a268f23784807142fba7a591dcb8b3e5.png" />
+        <Project
+          title="Book Nite"
+          github="https://github.com/abbyjo/book-nite"
+          link="https://abbyjo.github.io/book-nite/"
+          src="https://github.com/abbyjo/book-nite/raw/main/assets/images/screenshot.png"
+        />
+        <Project
+          title="Leap To Success"
+          github="https://github.com/akcodes29/Leap-To-Success"
+          link="https://github.com/akcodes29/Leap-To-Success"
+          src="https://i.gyazo.com/a268f23784807142fba7a591dcb8b3e5.png"
+        />
       </div>
       <div className='row'>
-      <Project title="Weather Dashboard" src="https://github.com/abbyjo/the-weather-up-there/raw/main/assets/images/screenshot.png" />
-      <Project title="Book Nite" src="https://miku.sega.com/megamixplus/img/characters/miku-full.jpg" />
+        <Project
+          title="Weather Dashboard"
+          github="https://github.com/abbyjo/the-weather-up-there"
+          link="https://abbyjo.github.io/the-weather-up-there/"
+          src="https://github.com/abbyjo/the-weather-up-there/raw/main/assets/images/screenshot.png"
+        />
+        <Project
+          title="Quiz Me"
+          github="https://github.com/abbyjo/quiz-me"
+          link="https://abbyjo.github.io/quiz-me/"
+          src="https://github.com/abbyjo/quiz-me/raw/main/assets/images/screenshot.png"
+        />
       </div>
       <div className="row">
-      <Project title="Book Nite" src="https://miku.sega.com/megamixplus/img/characters/miku-full.jpg" />
+        <Project
+          title="Project 6"
+          github=""
+          src="https://images.unsplash.com/photo-1682685796063-d2604827f7b3?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        />
       </div>
 
     </div>
